a3e5bfb99e88fc50255872bbf490546b07bf38a5
manuel.gay@ait.ac.at <manuel.gay@ait.ac.at>
2011-02-28 17:00:40 +0000

    removing remainders of obsolete helper mechanism

--- trunk/uim/api/src/main/java/eu/europeana/uim/api/AbstractIngestionPlugin.java
+++ trunk/uim/api/src/main/java/eu/europeana/uim/api/AbstractIngestionPlugin.java
@@ -1,18 +1,15 @@
 package eu.europeana.uim.api;
 
-import eu.europeana.uim.MDRFieldRegistry;
-import eu.europeana.uim.MetaDataRecord;
-import eu.europeana.uim.TKey;
-
 import java.util.Collections;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import eu.europeana.uim.MDRFieldRegistry;
+import eu.europeana.uim.TKey;
+
 /**
  * Abstract ingestion plugin providing defaults and helper methods for plugin implementation.
- * <br/>
- * FIXME we should remove the java.util.Logger and replace it with calls to the UIM Logger
  *
  * @author Andreas Juffinger (andreas.juffinger@kb.nl)
  * @author Manuel Bernhardt (bernhardt.manuel@gmail.com)
@@ -73,32 +70,5 @@ public abstract class AbstractIngestionPlugin implements IngestionPlugin {
     @Override
     public List<String> getParameters() {
         return Collections.EMPTY_LIST;
-    }
-
-    /**
-     * Helper class for easing common operations such as logging when implementing an @{IngestionPlugin}
-     */
-    public abstract static class IngestionPluginCall {
-        private final ExecutionContext context;
-        private final MetaDataRecord record;
-        private final IngestionPlugin plugin;
-
-        public IngestionPluginCall(MetaDataRecord record, ExecutionContext context, IngestionPlugin plugin) {
-            this.context = context;
-            this.record = record;
-            this.plugin = plugin;
-        }
-
-        public abstract void processRecord(MetaDataRecord record);
-
-        protected void log(LoggingEngine.Level level, String message) {
-            context.getLoggingEngine().log(level, message, context.getExecution(), record, plugin);
-        }
-
-        protected <T> void logStructured(LoggingEngine.Level level, T payload) {
-            @SuppressWarnings("unchecked")
-            LoggingEngine<T> loggingEngine = (LoggingEngine<T>) context.getLoggingEngine();
-            loggingEngine.logStructured(level, payload, context.getExecution(), record, plugin);
-        }
     }
 }
