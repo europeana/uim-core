d338eb16553eb7aa86b520e46f11db342232537d
andreas.juffinger@kb.nl <andreas.juffinger@kb.nl>
2011-03-20 10:08:40 +0000

    added utils

---
+++ trunk/uim/common/src/main/java/eu/europeana/uim/common/ArrayUtils.java
@@ -0,0 +1,45 @@
+/* ArrayUtils.java - created on Mar 20, 2011, Copyright (c) 2011 The European Library, all rights reserved */
+package eu.europeana.uim.common;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+ * 
+ * @author Andreas Juffinger (andreas.juffinger@kb.nl)
+ * @date Mar 20, 2011
+ */
+public class ArrayUtils {
+
+    public static List<long[]> batches(long[] data, int batchsize) {
+        ArrayList<long[]> result = new ArrayList<long[]>();
+
+        int batches = (int)Math.ceil(1.0 * data.length / batchsize);
+        for (int i = 0; i < batches; i++) {
+            int end = Math.min(data.length, (i + 1) * batchsize);
+            int start = i * batchsize;
+
+            long[] batch = new long[end - start];
+            System.arraycopy(data, start, batch, 0, end - start);
+            result.add(batch);
+        }
+        return result;
+    }
+
+    public static List<String[]> batches(String[] data, int batchsize) {
+        ArrayList<String[]> result = new ArrayList<String[]>();
+
+        int batches = (int)Math.ceil(1.0 * data.length / batchsize);
+        for (int i = 0; i < batches; i++) {
+            int end = Math.min(data.length, (i + 1) * batchsize);
+            int start = i * batchsize;
+
+            String[] batch = new String[end - start];
+            System.arraycopy(data, start, batch, 0, end - start);
+            result.add(batch);
+        }
+        return result;
+    }
+
+}
---
+++ trunk/uim/common/src/test/java/eu/europeana/uim/common/TestArrayUtils.java
@@ -0,0 +1,61 @@
+/* TestArrayUtils.java - created on Mar 20, 2011, Copyright (c) 2011 The European Library, all rights reserved */
+package eu.europeana.uim.common;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Test;
+
+/**
+ * 
+ * 
+ * @author Andreas Juffinger (andreas.juffinger@kb.nl)
+ * @date Mar 20, 2011
+ */
+public class TestArrayUtils {
+
+    
+    @Test
+    public void testLongBatches() {
+        long[] data = new long[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        
+        List<long[]> batches = ArrayUtils.batches(data, 3);
+        assertEquals(4, batches.size());
+        assertEquals(3, batches.get(0).length);
+        assertEquals(3, batches.get(1).length);
+        assertEquals(3, batches.get(2).length);
+        assertEquals(1, batches.get(3).length);
+        
+        batches = ArrayUtils.batches(data, 5);
+        assertEquals(2, batches.size());
+        assertEquals(5, batches.get(0).length);
+        assertEquals(5, batches.get(1).length);
+        
+        batches = ArrayUtils.batches(data, 12);
+        assertEquals(1, batches.size());
+        assertEquals(10, batches.get(0).length);
+    }
+
+    @Test
+    public void testStringBatches() {
+        String[] data = new String[] {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+        
+        List<String[]> batches = ArrayUtils.batches(data, 3);
+        assertEquals(4, batches.size());
+        assertEquals(3, batches.get(0).length);
+        assertEquals(3, batches.get(1).length);
+        assertEquals(3, batches.get(2).length);
+        assertEquals(1, batches.get(3).length);
+        
+        batches = ArrayUtils.batches(data, 5);
+        assertEquals(2, batches.size());
+        assertEquals(5, batches.get(0).length);
+        assertEquals(5, batches.get(1).length);
+        
+        batches = ArrayUtils.batches(data, 12);
+        assertEquals(1, batches.size());
+        assertEquals(10, batches.get(0).length);
+    }
+
+}
