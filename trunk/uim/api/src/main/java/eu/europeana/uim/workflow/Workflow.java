1690682057ab37a9a31acfb70b3ff8671f90f668
markus.muhr@kb.nl <markus.muhr@kb.nl>
2011-05-20 09:06:07 +0000

    no identifier;

--- trunk/uim/api/src/main/java/eu/europeana/uim/workflow/Workflow.java
+++ trunk/uim/api/src/main/java/eu/europeana/uim/workflow/Workflow.java
@@ -7,7 +7,6 @@ import eu.europeana.uim.api.IngestionPlugin;
 /**
  * Definition of a Unified Ingestion Management workflow consisting of a defined
  * {@link WorkflowStart}, followed by an arbitrary number of {@link IngestionPlugin}. Each workflow
- * provides an identifier (fully class name for example) to recognize them, furthermore a more
  * meaningful name together with a detailled description.
  * 
  * @author Manuel Bernhardt <bernhardt.manuel@gmail.com>
@@ -15,11 +14,6 @@ import eu.europeana.uim.api.IngestionPlugin;
  * @since May 20, 2011
  */
 public interface Workflow {
-// /**
-// * @return unique identifier of this workflow (suggestions is full class name)
-// */
-// String getIdentifier();
-
     /**
      * @return name of the workflow, should be reasonable meaningful
      */
