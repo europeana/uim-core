4d1e8422e627d8ae4097c2d99ff6b7845f036905
rene.wiermer@kb.nl <rene.wiermer@kb.nl>
2012-02-24 09:55:09 +0000

    Simplify code by caching the translations only at one place if possible (property files) or use direct access  to the provider (database via EhCache)

--- trunk/uim/common/src/main/java/org/theeuropeanlibrary/translation/Translations.java
+++ trunk/uim/common/src/main/java/org/theeuropeanlibrary/translation/Translations.java
@@ -16,7 +16,6 @@ import com.google.common.collect.MapMaker;
  */
 public class Translations {
 
-    private static String                                                  NO_TRANSLATION     = "NO_TRANSLATION";
 
     private static ConcurrentMap<TranslationProvider, TranslationProvider> providers          = new ConcurrentHashMap<TranslationProvider, TranslationProvider>();
 
@@ -44,19 +43,6 @@ public class Translations {
           }
       });
 
-    private static ConcurrentMap<CacheKey, String>            translationCache   = new MapMaker().makeComputingMap(new Function<CacheKey, String>() {
-          @Override
-          public String apply(CacheKey key) {
-              for (Locale alt : localeAlternatives.get(key.getLocale())) {
-                  for (TranslationProvider provider : providers.keySet()) {
-                      String translation = provider.getTranslation(key.getKey(), alt);
-                      if (translation != null && !translation.trim().equals("")) { return translation; }
-                  }
-              }
-              return NO_TRANSLATION;
-          }
-      });
-
     private Translations() {
     }
 
@@ -67,7 +53,6 @@ public class Translations {
      */
     public static void register(TranslationProvider provider) {
         providers.put(provider, provider);
-        translationCache.clear();
     }
 
     /**
@@ -77,7 +62,6 @@ public class Translations {
      */
     public static void unregister(TranslationProvider provider) {
         providers.remove(provider);
-        translationCache.clear();
     }
 
     /**
@@ -96,63 +80,15 @@ public class Translations {
      * @return translated string, or defaultValue
      */
     public static String getTranslation(String key, Locale locale, String defaultValue) {
-        String string = translationCache.get(new CacheKey(key, locale));
-        if (string != NO_TRANSLATION) {
-            return string;
-        } else {
-            return defaultValue;
-        }
-    }
-
-    static final class CacheKey  {
-        private final String key;
-        private final Locale locale;
-
-        public CacheKey(String key, Locale locale) {
-            super();
-            this.key = key;
-            this.locale = locale != null ? locale : Locale.ENGLISH;
-        }
         
-        /**
-         * Returns the key.
-         * @return the key
-         */
-        public String getKey() {
-            return key;
+        Locale localeNullSafe= locale !=null ? locale:Locale.ENGLISH;
+        for (Locale alt : localeAlternatives.get(localeNullSafe)) {
+            for (TranslationProvider provider : providers.keySet()) {
+                String translation = provider.getTranslation(key, alt);
+                if (translation != null && !translation.trim().equals("")) { return translation; }
+            }
         }
-
-        /**
-         * Returns the locale.
-         * @return the locale
-         */
-        public Locale getLocale() {
-            return locale;
-        }
-
-        @Override
-        public int hashCode() {
-            final int prime = 31;
-            int result = 1;
-            result = prime * result + ((key == null) ? 0 : key.hashCode());
-            result = prime * result + ((locale == null) ? 0 : locale.hashCode());
-            return result;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) return true;
-            if (obj == null) return false;
-            if (getClass() != obj.getClass()) return false;
-            CacheKey other = (CacheKey)obj;
-            if (key == null) {
-                if (other.key != null) return false;
-            } else if (!key.equals(other.key)) return false;
-            if (locale == null) {
-                if (other.locale != null) return false;
-            } else if (!locale.equals(other.locale)) return false;
-            return true;
-        }
-
+            return defaultValue;
     }
+
 }
