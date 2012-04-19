17fd7d8ec4dbf8abf84ad0b623eebf799f10c8fe
markus.muhr@kb.nl <markus.muhr@kb.nl>
2012-04-19 07:29:11 +0000

    formatting;

--- trunk/uim/model/common/src/main/java/org/theeuropeanlibrary/model/common/qualifier/ResourceType.java
+++ trunk/uim/model/common/src/main/java/org/theeuropeanlibrary/model/common/qualifier/ResourceType.java
@@ -2,150 +2,137 @@
 package org.theeuropeanlibrary.model.common.qualifier;
 
 /**
- * Types of content and material 
+ * Types of content and material
  * 
  * @author Nuno Freire (nfreire@gmail.com)
  * @date 29 de Ago de 2011
  */
 public enum ResourceType {
-	/** 
-	 *	MARC21 code 'a', 't'
-	 *  UNIMARC code 'a', 'b'
-	 */
-	TEXT,
-    
-	/** 
-	 *	MARC21 code 'e', 'f'
-	 *  UNIMARC code 'e', 'f'
-	 */
-	CARTOGRAPHIC,
-	
-	/** 
-	 *	MARC21 code 'c', 'd'
-	 *  UNIMARC code 'c', 'd'
-	 */
-	NOTATED_MUSIC,
-	
-	/** 
-	 *	MARC21 code 'i'
-	 *  UNIMARC code 'i'
-	 */
-	SOUND_RECORDING,
-	
-	/** 
-	 *	MARC21 code 'j'
-	 *  UNIMARC code 'j'
-	 */
-	MUSIC_SOUND_RECORDING,
+    /**
+     * MARC21 code 'a', 't' UNIMARC code 'a', 'b'
+     */
+    TEXT,
 
-	/** 
-	 *	MARC21 code 'k'
-	 *  UNIMARC code 'k'
-	 */
-	STILL_IMAGE,
-	
-	/** 
-	 *	MARC21 code 'g'
-	 *  UNIMARC code 'g'
-	 */
-	VIDEO,
-	
-	/** 
-	 *	MARC21 code 'r'
-	 *  UNIMARC code 'r'
-	 */
-	THREE_DIMENSIONAL_OBJECT,
-	
-	/** 
-	 *	MARC21 code 'm'
-	 *  UNIMARC code 'l'
-	 */
-	ELECTRONIC_RESOURCE,
-	
-	/** 
-	 *	MARC21 code -
-	 *  UNIMARC code 'm'
-	 */
-	MULTIMEDIA,
+    /**
+     * MARC21 code 'e', 'f' UNIMARC code 'e', 'f'
+     */
+    CARTOGRAPHIC,
 
-	/** 
-	 *	MARC21 code 'p'
-	 *  UNIMARC code -
-	 */
-	MIXED_MATERIALS;
-	
-	
-	/**
-	 * @param typeOfRecordCode
-	 * @return ResourceType
-	 */
-	public static ResourceType fromMarc21Code(char typeOfRecordCode) {
-		switch (typeOfRecordCode) {
-		case 'a':
-			return TEXT;
-		case 't':
-			return TEXT;
-		case 'e':
-			return CARTOGRAPHIC;
-		case 'f':
-			return CARTOGRAPHIC;
-		case 'c':
-			return NOTATED_MUSIC;
-		case 'd':
-			return NOTATED_MUSIC;
-		case 'i':
-			return SOUND_RECORDING;
-		case 'j':
-			return MUSIC_SOUND_RECORDING;
-		case 'k':
-			return STILL_IMAGE;
-		case 'g':
-			return VIDEO;
-		case 'r':
-			return THREE_DIMENSIONAL_OBJECT;
-		case 'm':
-			return ELECTRONIC_RESOURCE;
-		case 'o'://KIT
-		case 'p':
-			return MIXED_MATERIALS;
-		}
-		return null;
-	}
-	
-	
-	/**
-	 * @param typeOfRecordCode
-	 * @return ResourceType
-	 */
-	public static ResourceType fromUnimarcCode(char typeOfRecordCode) {
-		switch (typeOfRecordCode) {
-		case 'a':
-			return TEXT;
-		case 'b':
-			return TEXT;
-		case 'e':
-			return CARTOGRAPHIC;
-		case 'f':
-			return CARTOGRAPHIC;
-		case 'c':
-			return NOTATED_MUSIC;
-		case 'd':
-			return NOTATED_MUSIC;
-		case 'i':
-			return SOUND_RECORDING;
-		case 'j':
-			return MUSIC_SOUND_RECORDING;
-		case 'k':
-			return STILL_IMAGE;
-		case 'g':
-			return VIDEO;
-		case 'r':
-			return THREE_DIMENSIONAL_OBJECT;
-		case 'l':
-			return ELECTRONIC_RESOURCE;
-		case 'm':
-			return MULTIMEDIA;
-		}
-		return null;
-	}
+    /**
+     * MARC21 code 'c', 'd' UNIMARC code 'c', 'd'
+     */
+    NOTATED_MUSIC,
+
+    /**
+     * MARC21 code 'i' UNIMARC code 'i'
+     */
+    SOUND_RECORDING,
+
+    /**
+     * MARC21 code 'j' UNIMARC code 'j'
+     */
+    MUSIC_SOUND_RECORDING,
+
+    /**
+     * MARC21 code 'k' UNIMARC code 'k'
+     */
+    STILL_IMAGE,
+
+    /**
+     * MARC21 code 'g' UNIMARC code 'g'
+     */
+    VIDEO,
+
+    /**
+     * MARC21 code 'r' UNIMARC code 'r'
+     */
+    THREE_DIMENSIONAL_OBJECT,
+
+    /**
+     * MARC21 code 'm' UNIMARC code 'l'
+     */
+    ELECTRONIC_RESOURCE,
+
+    /**
+     * MARC21 code - UNIMARC code 'm'
+     */
+    MULTIMEDIA,
+
+    /**
+     * MARC21 code 'p' UNIMARC code -
+     */
+    MIXED_MATERIALS;
+
+    /**
+     * @param typeOfRecordCode
+     * @return ResourceType
+     */
+    public static ResourceType fromMarc21Code(char typeOfRecordCode) {
+        switch (typeOfRecordCode) {
+        case 'a':
+            return TEXT;
+        case 't':
+            return TEXT;
+        case 'e':
+            return CARTOGRAPHIC;
+        case 'f':
+            return CARTOGRAPHIC;
+        case 'c':
+            return NOTATED_MUSIC;
+        case 'd':
+            return NOTATED_MUSIC;
+        case 'i':
+            return SOUND_RECORDING;
+        case 'j':
+            return MUSIC_SOUND_RECORDING;
+        case 'k':
+            return STILL_IMAGE;
+        case 'g':
+            return VIDEO;
+        case 'r':
+            return THREE_DIMENSIONAL_OBJECT;
+        case 'm':
+            return ELECTRONIC_RESOURCE;
+        case 'o':// KIT
+        case 'p':
+            return MIXED_MATERIALS;
+        }
+        return null;
+    }
+
+    /**
+     * @param typeOfRecordCode
+     * @return ResourceType
+     */
+    public static ResourceType fromUnimarcCode(char typeOfRecordCode) {
+        switch (typeOfRecordCode) {
+        case 'a':
+            return TEXT;
+        case 'b':
+            return TEXT;
+        case 'e':
+            return CARTOGRAPHIC;
+        case 'f':
+            return CARTOGRAPHIC;
+        case 'c':
+            return NOTATED_MUSIC;
+        case 'd':
+            return NOTATED_MUSIC;
+        case 'i':
+            return SOUND_RECORDING;
+        case 'j':
+            return MUSIC_SOUND_RECORDING;
+        case 'k':
+            return STILL_IMAGE;
+        case 'g':
+            return VIDEO;
+        case 'r':
+            return THREE_DIMENSIONAL_OBJECT;
+        case 'l':
+            return ELECTRONIC_RESOURCE;
+        case 'm':
+            return MULTIMEDIA;
+        }
+        return null;
+    }
 }
