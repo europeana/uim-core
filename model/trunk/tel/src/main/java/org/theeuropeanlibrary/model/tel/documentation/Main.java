e0c39319c2ab02323408ef50a345273273244fcf
nfreire@gmail.com <nfreire@gmail.com>
2013-02-06 16:56:31 +0000

    small javadoc added

--- model/trunk/tel/src/main/java/org/theeuropeanlibrary/model/tel/documentation/Main.java
+++ model/trunk/tel/src/main/java/org/theeuropeanlibrary/model/tel/documentation/Main.java
@@ -2,7 +2,8 @@
 package org.theeuropeanlibrary.model.tel.documentation;
 
 /**
- * Script to execute the geeration of the doccumentations
+ * Script to execute the generation of the doccumentations
+ * It needs also project europeana-uim-model-common to be in the same eclipse workspace.
  * 
  * @author Nuno Freire (nfreire@gmail.com)
  * @since 23 de Fev de 2012
@@ -15,7 +16,7 @@ public class Main {
         String programName = "GenerateTelInternalDocumentationScript";
         String defaultDocletClassName = TelInternalDocumentationDoclet.class.getCanonicalName();
         String[] javaDocParams = new String[] { "-sourcepath",
-                "src/main/java;../common/src/main/java", "-private",
+                "src/main/java;../europeana-uim-model-common/src/main/java", "-private",
                 "org.theeuropeanlibrary.model.tel", "org.theeuropeanlibrary.model.tel.qualifier",
                 "org.theeuropeanlibrary.model.common", "org.theeuropeanlibrary.model.common.party",
                 "org.theeuropeanlibrary.model.common.qualifier",
