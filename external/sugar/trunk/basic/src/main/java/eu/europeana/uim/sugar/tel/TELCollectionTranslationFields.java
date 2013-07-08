60c90825b086ed3e698447516f61fab8daedef2c
markus.muhr@kb.nl <markus.muhr@kb.nl>
2013-07-08 08:11:04 +0000

    formatting;

--- external/sugar/trunk/basic/src/main/java/eu/europeana/uim/sugar/tel/TELCollectionTranslationFields.java
+++ external/sugar/trunk/basic/src/main/java/eu/europeana/uim/sugar/tel/TELCollectionTranslationFields.java
@@ -6,30 +6,39 @@ import eu.europeana.uim.sugar.model.RetrievableField;
 import eu.europeana.uim.sugar.model.UpdatableField;
 
 /**
- * 
+ * Defines fields from sugar for collections descriptions.
  * 
  * @author Rene Wiermer (rene.wiermer@kb.nl)
  * @date Sep 3, 2012
  */
-public enum  TELCollectionTranslationFields implements RetrievableField, UpdatableField {
-    /** TELCollectionTranslationFields LANGUAGE */
-    LANGUAGE("language_iso3","telda_TEL_collection_descriptions.language_iso3",null,"ISO3 string of the language"),
-    /** TELCollectionTranslationFields TITLE */
-    TITLE("title", "telda_TEL_collection_descriptions.title", null, "Localized title of the collection"),
-    /** TELCollectionTranslationFields DESCRIPTION */
-    DESCRIPTION("description", "telda_TEL_collection_descriptions.title", null, "Localized description of the collection");
-    
-    
+public enum TELCollectionTranslationFields implements RetrievableField, UpdatableField {
+    /** 
+     * language field from sugar
+     */
+    LANGUAGE("language_iso3", "telda_TEL_collection_descriptions.language_iso3", null,
+             "ISO3 string of the language"),
+    /** 
+     * title field from sugar
+     */
+    TITLE("title", "telda_TEL_collection_descriptions.title", null,
+          "Localized title of the collection"),
+    /** 
+     * description field from sugar
+     */
+    DESCRIPTION("description", "telda_TEL_collection_descriptions.title", null,
+                "Localized description of the collection");
+
     private final String                       fieldId;
     private final String                       qualifiedFieldId;
     private final ControlledVocabularyKeyValue key;
     private final String                       description;
-    
-    private TELCollectionTranslationFields(String fieldId,String qualifiedFieldId,ControlledVocabularyKeyValue key,String description) {
-        this.fieldId=fieldId;
-        this.qualifiedFieldId=qualifiedFieldId;
-        this.key=key;
-        this.description=description;
+
+    private TELCollectionTranslationFields(String fieldId, String qualifiedFieldId,
+                                           ControlledVocabularyKeyValue key, String description) {
+        this.fieldId = fieldId;
+        this.qualifiedFieldId = qualifiedFieldId;
+        this.key = key;
+        this.description = description;
     }
 
     @Override
@@ -51,5 +60,4 @@ public enum  TELCollectionTranslationFields implements RetrievableField, Updatab
     public String getDescription() {
         return description;
     }
-
 }
