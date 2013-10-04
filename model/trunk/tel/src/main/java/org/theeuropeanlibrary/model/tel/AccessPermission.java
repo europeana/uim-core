86928a20ebd6deec5762605d94553bd34e44ba99
Markus Muhr <markus.muhr@kb.nl>
2013-10-04 11:29:12 +0200

    formatting and empty constructor;

--- model/trunk/tel/src/main/java/org/theeuropeanlibrary/model/tel/AccessPermission.java
+++ model/trunk/tel/src/main/java/org/theeuropeanlibrary/model/tel/AccessPermission.java
@@ -1,29 +1,29 @@
 /* AccessPermissions.java - created on 11 de Set de 2013, Copyright (c) 2011 The European Library, all rights reserved */
 package org.theeuropeanlibrary.model.tel;
 
-
 import org.theeuropeanlibrary.model.common.FieldId;
 
 /**
- * Allow TEL to tag records for inclusion or exclusion in certain scenarios of sharing the record, such as Europeana, LOD, etc. 
+ * Allow TEL to tag records for inclusion or exclusion in certain scenarios of sharing the record,
+ * such as Europeana, LOD, etc.
  * 
  * @author Nuno Freire (nfreire@gmail.com)
  * @since 11 de Set de 2013
  */
 public class AccessPermission {
-
     /**
      * Access statement
      * 
      * @author Nuno Freire (nfreire@gmail.com)
      * @since 19/09/2013
      */
-    public enum Access{ 
-    /** Access DENY */
-    DENY, 
-    /** Access ALLOW */
-    ALLOW }
-    
+    public enum Access {
+        /** Access DENY */
+        DENY,
+        /** Access ALLOW */
+        ALLOW
+    }
+
     /**
      * a tag identifying a context where access to this record is being specified
      */
@@ -31,13 +31,21 @@ public class AccessPermission {
     protected String context;
 
     /**
-     * What kind of access permission should be applied in the context  
+     * What kind of access permission should be applied in the context
      */
     @FieldId(2)
     protected Access access;
 
     /**
+     * Creates a new instance of this class.
+     */
+    public AccessPermission() {
+        // nothing to do
+    }
+
+    /**
      * Returns the context.
+     * 
      * @return the context
      */
     public final String getContext() {
@@ -46,7 +54,9 @@ public class AccessPermission {
 
     /**
      * Sets the context
-     * @param context the context to set
+     * 
+     * @param context
+     *            the context to set
      */
     public final void setContext(String context) {
         this.context = context;
@@ -54,6 +64,7 @@ public class AccessPermission {
 
     /**
      * Returns the access.
+     * 
      * @return the access
      */
     public final Access getAccess() {
@@ -62,7 +73,9 @@ public class AccessPermission {
 
     /**
      * Sets the access
-     * @param access the access to set
+     * 
+     * @param access
+     *            the access to set
      */
     public final void setAccess(Access access) {
         this.access = access;
@@ -89,7 +102,4 @@ public class AccessPermission {
         } else if (!context.equals(other.context)) return false;
         return true;
     }
-    
-    
-    
 }
