797dd4d0a83341871e47fd460f61f8203bca837a
Nuno Freire <nfreire@gmail.com>
2013-12-10 09:08:31 +0000

    Redistribute metadata profile field added

--- external/sugar/trunk/api/src/main/java/eu/europeana/uim/sugar/SugarControlledVocabulary.java
+++ external/sugar/trunk/api/src/main/java/eu/europeana/uim/sugar/SugarControlledVocabulary.java
@@ -86,7 +86,10 @@ public enum SugarControlledVocabulary implements ControlledVocabularyKeyValue {
     COLLECTION_FIELD_VALIDATION,
 
     /** execution id of validation run */
-    COLLECTION_EDM_VALIDATION;
+    COLLECTION_EDM_VALIDATION,
+    
+    /** execution id of validation run */
+    COLLECTION_REDISTRIBUTE_PROFILE;
 
     /** SugarCRM date format */
     public static DateFormat DATEFORMAT = new SimpleDateFormat("yyyy-MM-dd");
