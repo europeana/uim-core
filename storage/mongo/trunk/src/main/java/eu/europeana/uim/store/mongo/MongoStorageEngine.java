c06e6644cdc1560ee5bdd67e92f5d32952116d25
geomark <georgios.markakis@kb.nl>
2014-02-06 15:54:04 +0100

    Mongo storage fixes

--- storage/mongo/trunk/src/main/java/eu/europeana/uim/store/mongo/MongoStorageEngine.java
+++ storage/mongo/trunk/src/main/java/eu/europeana/uim/store/mongo/MongoStorageEngine.java
@@ -216,6 +216,11 @@ public class MongoStorageEngine extends AbstractEngine implements
 	 */
 	@Override
 	public void completed(ExecutionContext<?, String> context) {
+		if(context.getDataSet() instanceof Collection){
+			Collection<?> coll = (Collection<?>) context.getDataSet();
+			flushCollectionMDRS(coll.getMnemonic());
+		}
+		
 	}
 
 	/*
