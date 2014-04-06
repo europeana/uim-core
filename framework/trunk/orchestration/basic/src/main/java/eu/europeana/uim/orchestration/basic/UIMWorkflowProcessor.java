ade32120da2cd6d6ff8da0c1d0e99e2cfbcb8e63
1nf1n1ty08 <markus.muhr@kb.nl>
2014-04-06 23:57:57 +0200

    less again;

--- framework/trunk/orchestration/basic/src/main/java/eu/europeana/uim/orchestration/basic/UIMWorkflowProcessor.java
+++ framework/trunk/orchestration/basic/src/main/java/eu/europeana/uim/orchestration/basic/UIMWorkflowProcessor.java
@@ -59,7 +59,7 @@ public class UIMWorkflowProcessor<I> implements Runnable {
     // FIXME: Updated these values, cannot handle more
     private int                                                             maxRunningExecutions = 100;
     private int                                                             maxTotalProgress     = 1000;
-    private int                                                             maxInProgress        = 250;
+    private int                                                             maxInProgress        = 200;
 
     /**
      * Creates a new instance of this class.
