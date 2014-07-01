9f2a021585b4c23be4722f32f107284310f76d8b
1nf1n1ty08 <drakash08>
2014-07-01 11:59:19 +0200

    added new values for collection type

--- model/tel/src/main/java/org/theeuropeanlibrary/model/tel/qualifier/CollectionType.java
+++ model/tel/src/main/java/org/theeuropeanlibrary/model/tel/qualifier/CollectionType.java
@@ -2,28 +2,27 @@
 package org.theeuropeanlibrary.model.tel.qualifier;
 
 /**
- * 
+ * Type of collections 
  * 
  * @author Andreas Juffinger (andreas.juffinger@kb.nl)
  * @date Jul 29, 2011
  */
+@SuppressWarnings("javadoc")
 public enum CollectionType {
-    /** catalogue */
     CATALOGUE,
-
-    /**  */
     COLLECTION,
-
-    /**  */
+    
     SPECIAL,
-
-    /**  */
     CREATOR,
-
-    /**  */
     CONTRIBUTOR,
-
-    /**  */
     PROVIDER,
-
+    
+    BIBLIOGRAPHY,
+    DATABASE,
+    DATASET,
+    FINDING_AIDS,
+    PASSTHROUGH,
+    PERSONAL_COLLECTION,
+    REPOSITORY,
+    VIRTUAL_EXHIBITION,
 }
