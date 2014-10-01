3eea1a2bdb40f197b1333d21340660e51ee43365
1nf1n1ty08 <drakash08>
2014-10-01 11:49:22 +0200

    updated numbers + scheduled and completed checks

--- framework/orchestration/basic/src/main/java/eu/europeana/uim/orchestration/basic/UIMWorkflowProcessor.java
+++ framework/orchestration/basic/src/main/java/eu/europeana/uim/orchestration/basic/UIMWorkflowProcessor.java
@@ -58,8 +58,8 @@ public class UIMWorkflowProcessor<I> implements Runnable {
 
     // FIXME: Updated these values, cannot handle more
     private int                                                             maxRunningExecutions = 10;
-    private int                                                             maxTotalProgress     = 100; // 100 5000;
-    private int                                                             maxInProgress        = 10; // 10 200;
+    private int                                                             maxTotalProgress     = 500; // 100 5000;
+    private int                                                             maxInProgress        = 100; // 10 200;
 
     /**
      * Creates a new instance of this class.
@@ -103,7 +103,7 @@ public class UIMWorkflowProcessor<I> implements Runnable {
                         int execProgress = execution.getProgressSize();
                         
 //                        if ((totalProgress < maxTotalProgress || active.size() * maxInProgress >= maxTotalProgress) && execProgress < maxInProgress) {
-                        if (totalProgress < maxTotalProgress && execProgress < maxInProgress) {
+                        if (totalProgress < maxTotalProgress && execProgress < maxInProgress && execution.getScheduledSize() - execution.getCompletedSize() < maxInProgress) {
                             // we ask the work flow start if we have more to do
                             WorkflowStart start = execution.getWorkflow().getStart();
                             newtasks = ensureTasksInProgress(execution, start, execProgress,
