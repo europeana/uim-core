0af034fb9800fead83fc201c789f901133e41399
1nf1n1ty08 <drakash08>
2015-03-02 11:57:01 +0100

    fixing logging database

--- integration/src/test/java/eu/europeana/uim/integration/LoggingDatabaseTest.java
+++ integration/src/test/java/eu/europeana/uim/integration/LoggingDatabaseTest.java
@@ -1,6 +1,12 @@
 package eu.europeana.uim.integration;
 
+import static org.ops4j.pax.exam.CoreOptions.maven;
 import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.features;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.karafDistributionConfiguration;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.keepRuntimeFolder;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.logLevel;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.editConfigurationFileExtend;
 
 import java.util.logging.Level;
 
@@ -13,19 +19,16 @@ import eu.europeana.uim.Registry;
 import eu.europeana.uim.logging.LoggingEngine;
 import java.io.File;
 import javax.inject.Inject;
-import static org.ops4j.pax.exam.CoreOptions.maven;
 import org.ops4j.pax.exam.junit.PaxExam;
 import org.ops4j.pax.exam.karaf.options.KarafDistributionOption;
-import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.karafDistributionConfiguration;
-import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.keepRuntimeFolder;
 import org.ops4j.pax.exam.options.MavenArtifactUrlReference;
 import org.ops4j.pax.exam.options.MavenUrlReference;
 
 /**
  * Integration test for UIM commands<br/>
- * Warning: /!\ if you do not want to be driven insane, do check -- twice -- if
- * you do NOT have a running Karaf instance somewhere on your system<br/>
- *
+ * Warning: /!\ if you do not want to be driven insane, do check -- twice -- if you do NOT have a
+ * running Karaf instance somewhere on your system<br/>
+ * 
  * @author Manuel Bernhardt
  * @author Markus Muhr (markus.muhr@theeuropeanlibrary.org)
  * @since Apr 7, 2014
@@ -37,36 +40,35 @@ public class LoggingDatabaseTest {
 
     @Configuration
     public Option[] config() {
-        MavenArtifactUrlReference karafUrl = maven()
-                .groupId("org.apache.karaf")
-                .artifactId("apache-karaf")
-                .version("3.0.0")
-                .type("tar.gz");
+        MavenArtifactUrlReference karafUrl = maven().groupId("org.apache.karaf").artifactId(
+                "apache-karaf").version("3.0.0").type("tar.gz");
 
-        MavenUrlReference karafStandardRepo = maven()
-                .groupId("org.apache.karaf.features")
-                .artifactId("standard")
-                .classifier("features")
-                .type("xml")
-                .versionAsInProject();
-        
-        return new Option[]{
-            // KarafDistributionOption.debugConfiguration("5005", true),
-            karafDistributionConfiguration().frameworkUrl(karafUrl).unpackDirectory(new File("target/exam")).useDeployFolder(false),
-            keepRuntimeFolder(),
-            KarafDistributionOption.features(karafStandardRepo, "scr"),
-            mavenBundle().groupId("eu.europeana").artifactId("europeana-uim-common").versionAsInProject().start(),
-            mavenBundle().groupId("eu.europeana").artifactId("europeana-uim-api").versionAsInProject().start(),
-            mavenBundle().groupId("eu.europeana").artifactId("europeana-uim-storage-memory").versionAsInProject().start(),
-            mavenBundle().groupId("eu.europeana").artifactId("europeana-uim-logging-memory").versionAsInProject().start(),
-            // FIXME: problems with database logging, right now used is only memory one
-//            mavenBundle().groupId("eu.europeana").artifactId("europeana-uim-logging-database").versionAsInProject().start(),
-        };
+        MavenUrlReference karafStandardRepo = maven().groupId("org.apache.karaf.features").artifactId(
+                "standard").classifier("features").type("xml").versionAsInProject();
+
+        return new Option[] {
+                // KarafDistributionOption.debugConfiguration("5005", true),
+                karafDistributionConfiguration().frameworkUrl(karafUrl).unpackDirectory(
+                        new File("target/exam")).useDeployFolder(false),
+                logLevel(LogLevel.INFO),
+                keepRuntimeFolder(),
+                features(karafStandardRepo, "scr"),
+                
+                editConfigurationFileExtend("etc/eu.europeana.uim.logging.cfg", "db.driverClass", "org.hsqldb.jdbcDriver"),
+                editConfigurationFileExtend("etc/eu.europeana.uim.logging.cfg", "db.dialect", "HSQL"),
+                editConfigurationFileExtend("etc/eu.europeana.uim.logging.cfg", "db.jdbcUrl", "jdbc:hsqldb:mem:embedded"),
+                editConfigurationFileExtend("etc/eu.europeana.uim.logging.cfg", "db.user", "sa"),
+                editConfigurationFileExtend("etc/eu.europeana.uim.logging.cfg", "db.password", ""),
+                editConfigurationFileExtend("etc/eu.europeana.uim.logging.cfg", "db.showsql", "false"),
+                
+                mavenBundle().groupId("eu.europeana").artifactId("europeana-uim-common").versionAsInProject().start(),
+                mavenBundle().groupId("eu.europeana").artifactId("europeana-uim-api").versionAsInProject().start(),
+                mavenBundle().groupId("eu.europeana").artifactId("europeana-uim-logging-database").versionAsInProject().start(), };
     }
 
     /**
      * Tests logging.
-     *
+     * 
      * @throws Exception
      */
     @Test
