77158045ce2432fec078916efc7810f600eaf52f
ymamakis <yorgos.mamakis@europeana.eu>
2015-07-29 16:41:42 +0200

    Added Neo4j TKEY

--- model/europeana/src/main/java/eu/europeana/uim/model/europeana/EuropeanaModelRegistry.java
+++ model/europeana/src/main/java/eu/europeana/uim/model/europeana/EuropeanaModelRegistry.java
@@ -27,92 +27,100 @@ import eu.europeana.uim.common.TKey;
 
 /**
  * TKeys Definitions for the Europeana Data Model
- * 
+ *
  * @author Georgios Markakis
  */
 public final class EuropeanaModelRegistry {
 
-	/**
-	 * The key for all sorts of concepts.
-	 */
-	public static final TKey<EuropeanaModelRegistry, String> UNCLASSIFIED = TKey
-			.register(EuropeanaModelRegistry.class, "unclassified",
-					String.class);
-	/**
-	 * The key for a full EDM Record representation
-	 */
-	public static final TKey<EuropeanaModelRegistry, String> EDMRECORD = TKey
-			.register(EuropeanaModelRegistry.class, "edmrecord", String.class);
-	/**
-	 * The key for a dereferenced EDM Record
-	 */
-	public static final TKey<EuropeanaModelRegistry, String> EDMDEREFERENCEDRECORD = TKey
-			.register(EuropeanaModelRegistry.class, "edmdereferencedrecord",
-					String.class);
-	/**
+    /**
+     * The key for all sorts of concepts.
+     */
+    public static final TKey<EuropeanaModelRegistry, String> UNCLASSIFIED = TKey
+            .register(EuropeanaModelRegistry.class, "unclassified",
+                    String.class);
+    /**
+     * The key for a full EDM Record representation
+     */
+    public static final TKey<EuropeanaModelRegistry, String> EDMRECORD = TKey
+            .register(EuropeanaModelRegistry.class, "edmrecord", String.class);
+    /**
+     * The key for a dereferenced EDM Record
+     */
+    public static final TKey<EuropeanaModelRegistry, String> EDMDEREFERENCEDRECORD = TKey
+            .register(EuropeanaModelRegistry.class, "edmdereferencedrecord",
+                    String.class);
+    /**
      * The key for id redirects
      */
     public static final TKey<EuropeanaModelRegistry, EuropeanaRedirectId> EDMRECORDREDIRECT = TKey
             .register(EuropeanaModelRegistry.class, "edmrecordredirect",
                     EuropeanaRedirectId.class);
-	/**
-	 * The key date of dereference
-	 */
-	public static final TKey<EuropeanaModelRegistry, String> UIMDEREFERENCE = TKey
-			.register(EuropeanaModelRegistry.class, "uimdereferencet",
-					String.class);
+    /**
+     * The key date of dereference
+     */
+    public static final TKey<EuropeanaModelRegistry, String> UIMDEREFERENCE = TKey
+            .register(EuropeanaModelRegistry.class, "uimdereferencet",
+                    String.class);
 
-	/**
-	 * The key indicating the actual ingestion/update datde for a specific
-	 * record
-	 */
-	public static final TKey<EuropeanaModelRegistry, String> UIMINGESTIONDATE = TKey
-			.register(EuropeanaModelRegistry.class, "uimingestiondate",
-					String.class);
+    /**
+     * The key indicating the actual ingestion/update datde for a specific
+     * record
+     */
+    public static final TKey<EuropeanaModelRegistry, String> UIMINGESTIONDATE = TKey
+            .register(EuropeanaModelRegistry.class, "uimingestiondate",
+                    String.class);
 
-	/**
-	 * The key indicating the actual ingestion/update datde for a specific
-	 * record
-	 */
-	public static final TKey<EuropeanaModelRegistry, String> UIMUPDATEDDATE = TKey
-			.register(EuropeanaModelRegistry.class, "uimupdateddate",
-					String.class);
+    /**
+     * The key indicating the actual ingestion/update datde for a specific
+     * record
+     */
+    public static final TKey<EuropeanaModelRegistry, String> UIMUPDATEDDATE = TKey
+            .register(EuropeanaModelRegistry.class, "uimupdateddate",
+                    String.class);
 
-	/**
-	 * The key for a full EDM Record representation
-	 */
-	public static final TKey<EuropeanaModelRegistry, EuropeanaLink> EUROPEANALINK = TKey
-			.register(EuropeanaModelRegistry.class, "europeanalink",
-					EuropeanaLink.class);
+    /**
+     * The key for a full EDM Record representation
+     */
+    public static final TKey<EuropeanaModelRegistry, EuropeanaLink> EUROPEANALINK = TKey
+            .register(EuropeanaModelRegistry.class, "europeanalink",
+                    EuropeanaLink.class);
 
-	/**
-	 * A key indicating the status for a specific record
-	 */
-	public static final TKey<EuropeanaModelRegistry, Status> STATUS = TKey
-			.register(EuropeanaModelRegistry.class, "status", Status.class);
+    /**
+     * A key indicating the status for a specific record
+     */
+    public static final TKey<EuropeanaModelRegistry, Status> STATUS = TKey
+            .register(EuropeanaModelRegistry.class, "status", Status.class);
 
-	/**
-	 * A key indicating that a record has been removed from production and when
-	 */
-	public static final TKey<EuropeanaModelRegistry, Long> REMOVED = TKey
-			.register(EuropeanaModelRegistry.class, "removed", Long.class);
+    /**
+     * A key indicating that a record has been removed from production and when
+     */
+    public static final TKey<EuropeanaModelRegistry, Long> REMOVED = TKey
+            .register(EuropeanaModelRegistry.class, "removed", Long.class);
 
-	/**
-	 * A key indicating when a record was initially saved in the server index
-	 */
-	public static final TKey<EuropeanaModelRegistry, Long> INITIALSAVE = TKey
-			.register(EuropeanaModelRegistry.class, "initial", Long.class);
+    /**
+     * A key indicating when a record was initially saved in the server index
+     */
+    public static final TKey<EuropeanaModelRegistry, Long> INITIALSAVE = TKey
+            .register(EuropeanaModelRegistry.class, "initial", Long.class);
 
-	/**
-	 * A key indicating when a record was updated in the server index
-	 */
-	public static final TKey<EuropeanaModelRegistry, Long> UPDATEDSAVE = TKey
-			.register(EuropeanaModelRegistry.class, "updatedsave", Long.class);
+    /**
+     * A key indicating when a record was updated in the server index
+     */
+    public static final TKey<EuropeanaModelRegistry, Long> UPDATEDSAVE = TKey
+            .register(EuropeanaModelRegistry.class, "updatedsave", Long.class);
 
-	/**
-	 * A key to represent the enriched EDM
-	 */
-	public static final TKey<EuropeanaModelRegistry, String> EDMENRICHEDRECORD = TKey
-			.register(EuropeanaModelRegistry.class, "edmenrichedrecord",
-					String.class);
+    /**
+     * A key to represent the enriched EDM
+     */
+    public static final TKey<EuropeanaModelRegistry, String> EDMENRICHEDRECORD = TKey
+            .register(EuropeanaModelRegistry.class, "edmenrichedrecord",
+                    String.class);
+
+    /**
+     * A key to check whether the HO plugin was called
+     */
+    public static final TKey<EuropeanaModelRegistry, Boolean> ISHIERARCHY = TKey
+            .register(EuropeanaModelRegistry.class, "ishierarchy", Boolean.class);
+
+
 }
